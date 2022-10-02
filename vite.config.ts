821aa3706bbd65b258fe724ddab5173833859eb8
nrophi <nrophi@protonmail.com>
2022-10-03 01:18:00 +0300

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Расписание',
+        short_name: 'Расписание',
+        description: 'Расписание',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
