d9377148bd3f90d73410ff9034ce74d9c5089520
nrophi <nrophi@protonmail.com>
2022-10-02 22:01:52 +0300

    Add basic functionality
    * Removed unnecessary dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,177 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import schedule from "./assets/schedule.json";
+import ScheduleTable from "./components/ScheduleTable.vue";
+import { computed, ref } from "vue";
+
+const startFirstLecture = 8 * 60 + 15; // 8:15
+const sublecturePausingDuration = 5;
+const sublectureDuration = 45;
+const defaultPausingDuration = 10;
+const largePausingDuration = 30;
+const largePausingBeforeLecture = 1;
+const maxLectureCount = 5;
+
+enum Pausing {
+  None,
+  Lecture,
+  Sublecture,
+}
+
+const currentDate = ref(new Date());
+const isWeekend = computed(
+  () => [0, 6].indexOf(currentDate.value.getDay()) != -1
+);
+const currentMinutes = computed(() => {
+  return currentDate.value.getHours() * 60 + currentDate.value.getMinutes();
+});
+
+const studyWeekNum = computed(() => {
+  let weekCount = 0;
+  const year =
+    currentDate.value.getFullYear() - Number(currentDate.value.getMonth() < 8);
+  const date = new Date(year, 8, 1);
+  while (date <= currentDate.value) {
+    date.setDate(date.getDate() + (7 - date.getDay() + 1));
+    weekCount++;
+  }
+  return weekCount - 1;
+});
+
+const isSecondStudyWeek = computed(() => {
+  if (!studyWeekNum.value) return false;
+  return (studyWeekNum.value + 1) % 2;
+});
+
+const weekSchedule = computed(() => {
+  const weekSchedule = [];
+  for (const daySchedule of schedule) {
+    const weekdaySchedule = [];
+    for (const lecture of daySchedule) {
+      weekdaySchedule.push(lecture[+isSecondStudyWeek.value] || lecture[0]);
+    }
+    weekSchedule.push(weekdaySchedule);
+  }
+  return weekSchedule;
+});
+
+const currentTimeStatus = computed(() => {
+  let lectureNum = -1;
+  let sublectureNum = 0;
+  let pausing = Pausing.None;
+  let minutesLeft = currentMinutes.value - startFirstLecture;
+  let step = 0;
+  while (minutesLeft >= 0 && lectureNum < maxLectureCount) {
+    switch (step) {
+      case 0:
+        minutesLeft -= sublectureDuration;
+        pausing = Pausing.None;
+        step = sublectureNum ? 2 : 1;
+        break;
+      case 1:
+        minutesLeft -= sublecturePausingDuration;
+        pausing = Pausing.Sublecture;
+        sublectureNum = 1;
+        step = 0;
+        break;
+      case 2:
+        minutesLeft -=
+          lectureNum == largePausingBeforeLecture
+            ? largePausingDuration
+            : defaultPausingDuration;
+        pausing = Pausing.Lecture;
+        lectureNum++;
+        sublectureNum = 0;
+        step = 0;
+        break;
+    }
+  }
+  return {
+    lectureNum,
+    sublectureNum,
+    pausing,
+    minutesLeft: -minutesLeft,
+  };
+});
+
+const initLectureNum = computed(() => {
+  if (isWeekend.value) return -1;
+  let counter = 0;
+  while (
+    counter < maxLectureCount &&
+    Object.keys(weekSchedule.value[currentDate.value.getDay() - 1][counter])
+      .length === 0
+  )
+    counter++;
+  return counter;
+});
+
+const lastTimeForInitLecture = computed(() => {
+  if (isWeekend.value) return -1;
+  let minutesLeft = currentMinutes.value - startFirstLecture;
+  for (let i = 0; i < initLectureNum.value; i++) {
+    minutesLeft -= sublectureDuration * 2 + sublecturePausingDuration;
+    minutesLeft -=
+      i == largePausingBeforeLecture
+        ? largePausingDuration
+        : defaultPausingDuration;
+  }
+  return -minutesLeft;
+});
+
+setInterval(() => {
+  currentDate.value = new Date();
+}, 1000);
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="header">
+    <p>Текущее время: {{ currentDate.toLocaleString() }}</p>
+    <p>{{ isSecondStudyWeek ? "Вторая" : "Первая" }} учебная неделя</p>
+    <p>{{ studyWeekNum + 1 }} неделя занятий</p>
+    <p v-if="lastTimeForInitLecture > 0">
+      До начала занятий осталось: {{ lastTimeForInitLecture }} мин.
+    </p>
+    <template v-else-if="currentTimeStatus.minutesLeft >= 0">
+      <p>{{ currentTimeStatus.sublectureNum ? "II" : "I" }} полупара</p>
+      <p>
+        До {{ currentTimeStatus.pausing ? "начала" : "перерыва" }} осталось:
+        {{ currentTimeStatus.minutesLeft }} мин.
+      </p>
+    </template>
   </header>
-
-  <RouterView />
+  <div class="weekSchedule">
+    <div v-for="(daySchedule, index) in weekSchedule" :key="index">
+      <h3>
+        {{ ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"][index] }}
+      </h3>
+      <ScheduleTable
+        :schedule="daySchedule"
+        :activeNum="
+          currentDate.getDay() - 1 == index ? currentTimeStatus.lectureNum : -1
+        "
+      />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+<style scoped lang="scss">
+.header {
   text-align: center;
-  margin-top: 2rem;
+  margin: 50px 10px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+.weekSchedule {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 10px;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  h3 {
+    text-align: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  @media (max-width: 650px) {
+    align-items: unset;
   }
 }
 </style>
---
+++ src/components/ScheduleTable.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+const props = defineProps(["schedule", "activeNum"]);
+</script>
+
+<template>
+  <table class="schedule-table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Дисциплина</th>
+        <th>Преподаватель</th>
+        <th>Кабинет</th>
+        <th>Корпус</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        :class="{ active: props.activeNum == index }"
+        v-for="(
+          { disciplineName, teacherName, auditorium, building }, index
+        ) in props.schedule"
+        :key="index"
+      >
+        <td data-label="#">{{ index + 1 }}</td>
+        <td data-label="Дисциплина">{{ disciplineName }}</td>
+        <td data-label="Преподаватель">{{ teacherName }}</td>
+        <td data-label="Кабинет">{{ auditorium }}</td>
+        <td data-label="Корпус">{{ building }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped lang="scss">
+.schedule-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  width: 600px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+
+  thead tr {
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+  }
+
+  th,
+  td {
+    padding: 12px 15px;
+  }
+
+  tbody {
+    tr {
+      border-bottom: 1px solid #dddddd;
+
+      &:nth-of-type(even) {
+        background-color: #f3f3f3;
+      }
+
+      &.active {
+        font-weight: bold;
+        color: #009879;
+      }
+    }
+  }
+
+  @media (max-width: 650px) {
+    border: 0;
+    width: 100%;
+
+    thead {
+      border: none;
+      clip: rect(0 0 0 0);
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      padding: 0;
+      position: absolute;
+      width: 1px;
+    }
+
+    tr {
+      border-bottom: 3px solid #ddd;
+      display: block;
+      margin-bottom: 0.625em;
+    }
+
+    td {
+      border-bottom: 1px solid #ddd;
+      display: block;
+      font-size: 0.8em;
+      text-align: right;
+    }
+
+    td::before {
+      content: attr(data-label);
+      float: left;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+
+    td:last-child {
+      border-bottom: 0;
+    }
+  }
+}
+</style>
