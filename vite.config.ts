465ed9af0abde4a99c52ae9244b3b501ba7ec5cb
nrophi <nrophi@protonmail.com>
2022-10-28 20:58:48 +0300

    Add support GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -36,4 +36,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/university-schedule/" : "/",
 });
